7b0aea418a3e931b029f31fcce634ff0cf6ca6f3
shahrozijaz09 <sheributt094@gmail.com>
2025-01-11 12:36:46 +0000

    java

---
+++ java/j2.js
@@ -0,0 +1,22 @@
+let no = 145
+let someno = String(no)
+// console.log(someno)
+// console.log(typeof someno)
+
+// let no1 = 33
+
+
+
+// console.log(2*2no1);
+// console.log(2 + 2 * 3 % 5);
+
+let a = 0.2;
+let b = 
+
+console.log(++a)
+console.log(a++)
+console.log(++b)
+console.log(b++)
+//  string, number, boolean, non, undefined, symbol, bigint.
+// array, object, function.
+// array object function
--- java/java.js
+++ java/java.js
@@ -1,1 +1,6 @@
-console.log("shahroz")
+const acid = 1313
+let acname = 'shahroz'
+let accity = 'grw'
+let accountry = 'pakistan'
+// sdfvadf
+console.table({acid , acname , accity , accountry})
